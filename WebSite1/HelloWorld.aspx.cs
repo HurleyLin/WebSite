8da3858a5d02807fb47fe95aedf15390187f913e
lin0637 <1150118636@qq.com>
2018-09-06 10:34:07 +0800

    web的一些概念

---
+++ WebSite1/HelloWorld.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HelloWorld : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    public void ShowLineage()
+    {
+        Response.Write("Check out the family tree: <br/> <br/>");
+        Response.Write(this.GetType().ToString());
+        Response.Write(" which derives from: <br/> ");
+        Response.Write(this.GetType().BaseType.ToString());
+        Response.Write(" which derives from: <br/> ");
+        Response.Write(this.GetType().BaseType.BaseType.ToString());
+        Response.Write(" which derives from: <br/> ");
+        Response.Write(
+        this.GetType().BaseType.BaseType.BaseType.ToString());
+        Response.Write(" which derives from: <br/> ");
+        Response.Write(
+        this.GetType().BaseType.BaseType.BaseType.BaseType.ToString());
+    }
+}
